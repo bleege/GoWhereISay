cfe2f6ba32ea6034b36c3a4b09fc7fe169ba787a
Brad Leege <bleege@gmail.com>
2016-01-17 00:50:54 -0600

    Setting default location and zoom

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -2,14 +2,15 @@ package com.bradleege.gowhereisay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.setStyle(Style.MAPBOX_STREETS);
+        mapView.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(new LatLng(47.60865, -122.34052)).zoom(12).build()));
 
         mapView.onCreate(savedInstanceState);
     }
