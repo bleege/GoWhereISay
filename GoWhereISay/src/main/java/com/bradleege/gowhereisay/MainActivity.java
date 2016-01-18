e0cffe4ef6d90778f53c148b598be550d14c5dfa
Brad Leege <bleege@gmail.com>
2016-01-17 19:17:52 -0600

    Pike Place starting marker and FAB onClick handling

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -1,7 +1,11 @@
 package com.bradleege.gowhereisay;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
@@ -10,16 +14,30 @@ import com.mapbox.mapboxsdk.views.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
+
     private MapView mapView;
+    private static final LatLng pikePlaceLatLng = new LatLng(47.60865, -122.34052);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.setStyle(Style.MAPBOX_STREETS);
-        mapView.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(new LatLng(47.60865, -122.34052)).zoom(12).build()));
+        mapView.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(pikePlaceLatLng).zoom(12).build()));
+
+        mapView.addMarker(new MarkerOptions().position(pikePlaceLatLng).title("Pike Place Market"));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "FAB onClick()");
+            }
+        });
 
         mapView.onCreate(savedInstanceState);
     }
