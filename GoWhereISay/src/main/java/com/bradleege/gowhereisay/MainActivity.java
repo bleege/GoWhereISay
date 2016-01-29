efc1ffddd178faa7a383ab623cd66825c5a62bcc
Brad Leege <bleege@gmail.com>
2016-01-28 22:14:46 -0600

    Turning Speach into Coordinates via Geocoder to then get Directions with

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+import com.mapbox.geocoder.MapboxGeocoder;
+import com.mapbox.geocoder.service.models.GeocoderFeature;
+import com.mapbox.geocoder.service.models.GeocoderResponse;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -16,6 +19,10 @@ import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 import java.util.ArrayList;
+import java.util.List;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -121,16 +128,51 @@ public class MainActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
 
                     ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-                    for (String txt : text) {
-                        Log.i(TAG, "text = " + text);
+                    Log.i(TAG, "results text = " + text);
+                    if (text.size() > 0) {
+                        searchForDirections(text.get(0));
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "We had a failure to communicate.",
+                                Toast.LENGTH_SHORT).show();
                     }
-
                 }
                 break;
             }
 
         }
 
+    }
+
+    private void searchForDirections(final String endpoint) {
+
+        // Get Coordinates For Endpoint
+        MapboxGeocoder client = new MapboxGeocoder.Builder()
+                .setAccessToken(getString(R.string.mapbox_access_token))
+                .setLocation(endpoint)
+                .setProximity(pikePlaceLatLng.getLongitude(), pikePlaceLatLng.getLatitude())
+                .build();
+
+        client.enqueue(new Callback<GeocoderResponse>() {
+            @Override
+            public void onResponse(Response<GeocoderResponse> response, Retrofit retrofit) {
+
+                List<GeocoderFeature> results = response.body().getFeatures();
+                if (results.size() > 0) {
+                    Log.i(TAG, "Coordinates = " + results.get(0).getLatitude() + ", " + results.get(0).getLongitude());
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "Couldn't find a coordinate for '" + endpoint + "'",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(getApplicationContext(),
+                        "Failure occurred while trying to find a coordinate for '" + endpoint + "'",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
