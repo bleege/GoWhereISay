e9533bd637ca4a354f4054b7e5662897a9291a89
Brad Leege <bleege@gmail.com>
2016-01-28 21:31:51 -0600

    Initial setup of Speech to Text infrastructure

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -1,20 +1,27 @@
 package com.bradleege.gowhereisay;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
+
+    protected static final int RESULT_SPEECH = 1;
 
     private MapView mapView;
     private static final LatLng pikePlaceLatLng = new LatLng(47.60865, -122.34052);
@@ -23,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.setStyle(Style.MAPBOX_STREETS);
@@ -36,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i(TAG, "FAB onClick()");
+
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "This device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -86,5 +103,34 @@ public class MainActivity extends AppCompatActivity {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+    /**
+     * Dispatch incoming result to the correct fragment.
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    for (String txt : text) {
+                        Log.i(TAG, "text = " + text);
+                    }
+
+                }
+                break;
+            }
+
+        }
+
     }
 }
