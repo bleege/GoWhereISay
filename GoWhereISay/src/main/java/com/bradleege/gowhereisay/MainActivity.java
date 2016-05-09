5c07047bbc85fb2fbbcf77e54905a51388666134
Brad Leege <bleege@gmail.com>
2016-05-09 17:34:47 -0500

    Converting to Mapbox Android SDK 4.0.0 and MAS 1.1 code

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -2,34 +2,38 @@ package com.bradleege.gowhereisay;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.Color;
 import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-import com.mapbox.directions.DirectionsCriteria;
-import com.mapbox.directions.MapboxDirections;
-import com.mapbox.directions.service.models.DirectionsResponse;
-import com.mapbox.directions.service.models.DirectionsRoute;
-import com.mapbox.directions.service.models.Waypoint;
-import com.mapbox.geocoder.MapboxGeocoder;
-import com.mapbox.geocoder.service.models.GeocoderFeature;
-import com.mapbox.geocoder.service.models.GeocoderResponse;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
-import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.services.Constants;
+import com.mapbox.services.commons.ServicesException;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.directions.v5.DirectionsCriteria;
+import com.mapbox.services.directions.v5.MapboxDirections;
+import com.mapbox.services.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.directions.v5.models.DirectionsRoute;
+import com.mapbox.services.geocoding.v5.MapboxGeocoding;
+import com.mapbox.services.geocoding.v5.models.GeocodingFeature;
+import com.mapbox.services.geocoding.v5.models.GeocodingResponse;
 import java.util.ArrayList;
 import java.util.List;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected static final int RESULT_SPEECH = 1;
 
     private MapView mapView;
+    private MapboxMap mapboxMap;
     private static final LatLng pikePlaceLatLng = new LatLng(47.60865, -122.34052);
 
     @Override
@@ -46,15 +51,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mapView = (MapView) findViewById(R.id.mapView);
-        mapView.setStyle(Style.MAPBOX_STREETS);
-        resetMap();
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap map) {
+                mapboxMap = map;
+                resetMap();
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, "FAB onClick()");
-
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
 
@@ -67,14 +76,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
-        mapView.onCreate(savedInstanceState);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mapView.onStart();
     }
 
     @Override
@@ -88,13 +89,6 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         mapView.onPause();
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
-
 
     @Override
     protected void onDestroy() {
@@ -133,10 +127,14 @@ public class MainActivity extends AppCompatActivity {
                 if (resultCode == RESULT_OK && null != data) {
 
                     ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    Log.i(TAG, "results text = " + text);
                     if (text.size() > 0) {
                         resetMap();
-                        searchForEndpointCoordinates(text.get(0));
+                        try {
+                            searchForEndpointCoordinates(text.get(0));
+                        } catch (ServicesException e) {
+                            Log.e(TAG, "Exception While Searching:" + e.getMessage());
+                            Toast.makeText(MainActivity.this, "Error While Searching For Endpoint", Toast.LENGTH_SHORT).show();
+                        }
                     } else {
                         Toast.makeText(getApplicationContext(),
                                 "We had a failure to communicate.",
@@ -150,77 +148,84 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void searchForEndpointCoordinates(final String endpoint) {
+    private void searchForEndpointCoordinates(final String endpoint) throws ServicesException {
 
         // Get Coordinates For Endpoint
-        MapboxGeocoder client = new MapboxGeocoder.Builder()
-                .setAccessToken(getString(R.string.mapbox_access_token))
-                .setLocation(endpoint)
-                .setProximity(pikePlaceLatLng.getLongitude(), pikePlaceLatLng.getLatitude())
-                .build();
+        MapboxGeocoding client = new MapboxGeocoding.Builder()
+                    .setAccessToken(getString(R.string.mapbox_access_token))
+                    .setLocation(endpoint)
+                    .setProximity(Position.fromCoordinates(pikePlaceLatLng.getLongitude(), pikePlaceLatLng.getLatitude()))
+                    .build();
 
-        client.enqueue(new Callback<GeocoderResponse>() {
+        client.enqueueCall(new Callback<GeocodingResponse>() {
             @Override
-            public void onResponse(Response<GeocoderResponse> response, Retrofit retrofit) {
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
 
-                List<GeocoderFeature> results = response.body().getFeatures();
-                if (results.size() > 0) {
-                    Log.i(TAG, "Coordinates = " + results.get(0).getLatitude() + ", " + results.get(0).getLongitude());
-                    searchForDirections(results.get(0));
-                } else {
-                    Toast.makeText(getApplicationContext(),
-                            "Couldn't find a coordinate for '" + endpoint + "'",
-                            Toast.LENGTH_SHORT).show();
+                if (response != null && response.body() != null && response.body().getFeatures() != null) {
+                    List<GeocodingFeature> features = response.body().getFeatures();
+                    if (!features.isEmpty()) {
+                        Log.i(TAG, "Coordinates (Lat / Lon) = " + features.get(0).asPosition().getLatitude() + ", " + features.get(0).asPosition().getLongitude());
+                        try {
+                            searchForDirections(features.get(0));
+                        } catch (ServicesException e) {
+                            Log.e(TAG, "Exception While Searching for Directions: "+ e);
+                            Toast.makeText(MainActivity.this, "Error While Searching For Directions", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't find a coordinate for '" + endpoint + "'",
+                                Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
 
             @Override
-            public void onFailure(Throwable t) {
+            public void onFailure(Call<GeocodingResponse> call, Throwable t) {
                 Toast.makeText(getApplicationContext(),
                         "Failure occurred while trying to find a coordinate for '" + endpoint + "'",
                         Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 
-    private void searchForDirections(final GeocoderFeature endpointFeature) {
+    private void searchForDirections(final GeocodingFeature endpointFeature) throws ServicesException {
 
-        Waypoint origin = new Waypoint(pikePlaceLatLng.getLongitude(), pikePlaceLatLng.getLatitude());
-        Waypoint destination = new Waypoint(endpointFeature.getLongitude(), endpointFeature.getLatitude());
+        final ArrayList<Position> points = new ArrayList<>();
+        points.add(Position.fromCoordinates(pikePlaceLatLng.getLongitude(), pikePlaceLatLng.getLatitude()));
+        points.add(endpointFeature.asPosition());
 
-        MapboxDirections md = new MapboxDirections.Builder()
+        MapboxDirections client = new com.mapbox.services.directions.v5.MapboxDirections.Builder()
                 .setAccessToken(getString(R.string.mapbox_access_token))
-                .setOrigin(origin)
-                .setDestination(destination)
-                .setProfile(DirectionsCriteria.PROFILE_WALKING)
+                .setCoordinates(points)
+                .setProfile(DirectionsCriteria.PROFILE_DRIVING)
                 .build();
 
-        md.enqueue(new Callback<DirectionsResponse>() {
+        client.enqueueCall(new Callback<DirectionsResponse>() {
             @Override
-            public void onResponse(Response<DirectionsResponse> response, Retrofit retrofit) {
+            public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+                if (response != null && response.body() != null) {
+                    DirectionsRoute route = response.body().getRoutes().get(0);
 
-                // Print some info about the route
-                DirectionsRoute firstRoute = response.body().getRoutes().get(0);
-                Log.i(TAG, "Distance: " + firstRoute.getDistance());
-                Log.i(TAG, (String.format("Route is %d meters long.", firstRoute.getDistance())));
+                    resetMap();
 
-                resetMap();
+                    mapboxMap.addMarker(new MarkerOptions().position(new LatLng(endpointFeature.asPosition().getLatitude(), endpointFeature.asPosition().getLongitude())).title(endpointFeature.getPlaceName()).snippet(endpointFeature.getText()));
 
-                // Put the Destination on the map
-                mapView.addMarker(new MarkerOptions().position(new LatLng(endpointFeature.getLatitude(), endpointFeature.getLongitude())).title(endpointFeature.getPlaceName()).snippet(endpointFeature.getText()));
+                    // Center Route On Map
+                    LatLng center = new LatLng(
+                            (pikePlaceLatLng.getLatitude() + endpointFeature.asPosition().getLatitude()) / 2,
+                            (pikePlaceLatLng.getLongitude() + endpointFeature.asPosition().getLongitude()) / 2);
+                    mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(center).build()));
 
-                // Center Route On Map
-                LatLng center = new LatLng(
-                        (pikePlaceLatLng.getLatitude() + endpointFeature.getLatitude()) / 2,
-                        (pikePlaceLatLng.getLongitude() + endpointFeature.getLongitude()) / 2);
-                mapView.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(center).build()));
-
-                // Draw the route on the map
-                drawRoute(firstRoute);
+                    // Draw the route on the map
+                    drawRoute(route);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No routes could be found.", Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
-            public void onFailure(Throwable t) {
+            public void onFailure(Call<DirectionsResponse> call, Throwable t) {
                 Toast.makeText(getApplicationContext(),
                         "Failure occurred while trying to find directions for '" + endpointFeature.getPlaceName() + "'",
                         Toast.LENGTH_SHORT).show();
@@ -230,27 +235,26 @@ public class MainActivity extends AppCompatActivity {
 
     private void drawRoute(DirectionsRoute route) {
 
-        // Convert List<Waypoint> into LatLng[]
-        List<Waypoint> waypoints = route.getGeometry().getWaypoints();
-        LatLng[] point = new LatLng[waypoints.size()];
-        for (int i = 0; i < waypoints.size(); i++) {
-            point[i] = new LatLng(
-                    waypoints.get(i).getLatitude(),
-                    waypoints.get(i).getLongitude());
+        // show route
+        List<PolylineOptions> polylineOptions = new ArrayList<>();
+        PolylineOptions builder = new PolylineOptions();
+        builder.color(ContextCompat.getColor(this, R.color.colorAccent));
+        builder.alpha(0.5f);
 
+        LineString lineString = LineString.fromPolyline(route.getGeometry(), Constants.OSRM_PRECISION_V5);
+        List<Position> coordinates = lineString.getCoordinates();
+        for (int lc = 0; lc < coordinates.size(); lc++) {
+            builder.add(new LatLng(coordinates.get(lc).getLatitude(), coordinates.get(lc).getLongitude()));
         }
 
-        // Draw Points on MapView
-        mapView.addPolyline(new PolylineOptions()
-                .add(point)
-                .color(Color.parseColor("#3887be"))
-                .width(5));
+        builder.width(getResources().getDimension(R.dimen.line_width_route));
+        polylineOptions.add(builder);
+        mapboxMap.addPolylines(polylineOptions);
     }
 
     private void resetMap() {
-        mapView.removeAllAnnotations();
-        mapView.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(pikePlaceLatLng).zoom(12).build()));
-
-        mapView.addMarker(new MarkerOptions().position(pikePlaceLatLng).title("Pike Place Market"));
+        mapboxMap.removeAnnotations();
+        mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder().target(pikePlaceLatLng).zoom(12).build()));
+        mapboxMap.addMarker(new MarkerOptions().position(pikePlaceLatLng).title("Pike Place Market"));
     }
 }
