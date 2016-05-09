5bba3d56c9f402075ec62feca3d1a8188dac36b0
Brad Leege <bleege@gmail.com>
2016-05-09 17:59:10 -0500

    Adding Reset Toolbar Action

--- GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
+++ GoWhereISay/src/main/java/com/bradleege/gowhereisay/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -109,6 +111,27 @@ public class MainActivity extends AppCompatActivity {
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_reset) {
+            if (mapboxMap != null) {
+                resetMap();
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
